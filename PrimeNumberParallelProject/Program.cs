6287a40347aeb5c25c3d183e1ef710a3b02175fd
desmoswal <mark.palmai94@gmail.com>
2019-10-10 11:52:23 +0200

    First Commit

---
+++ PrimeNumberParallelProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeNumberParallelProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
